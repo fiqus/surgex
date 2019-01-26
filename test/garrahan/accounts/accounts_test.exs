64818414c49a287c6f5f181b65b884f8299d685c
Diego Calero <dcalero@fiqus.com>
2019-01-26 16:17:12 -0300

    Making accounts_test to work.

--- test/garrahan/accounts/accounts_test.exs
+++ test/garrahan/accounts/accounts_test.exs
@@ -2,45 +2,37 @@ defmodule Garrahan.AccountsTest do
   use Garrahan.DataCase
 
   alias Garrahan.Accounts
+  alias Garrahan.Accounts.User
 
   describe "users" do
-    alias Garrahan.Accounts.User
 
-    @valid_attrs %{email: "some email", password: "some password"}
-    @update_attrs %{email: "some updated email", password: "some updated password"}
-    @invalid_attrs %{email: nil, password: nil}
+    @valid_attrs %{password: "some password", is_admin: true}
+    @update_attrs %{password: "some updated password", is_admin: false}
+    @invalid_attrs %{password: nil}
 
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_user()
-
-      user
+    setup do
+      {:ok, user} = Accounts.create_user(@valid_attrs)
+      {:ok, user: user}
     end
 
     defp are_same_user(user1, user2) do
       assert user1.id == user2.id
-      assert user1.email == user2.email
     end
 
-    test "list_users/0 returns all users" do
-      user = user_fixture()
-
+    test "list_users/0 returns all users", %{user: user} do
       Accounts.list_users()
       |> Enum.map(fn u ->
         are_same_user(u, user)
       end)
     end
 
-    test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+    test "get_user!/1 returns the user with given id", %{user: user} do
       are_same_user(Accounts.get_user!(user.id), user)
     end
 
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.email == "some email"
+      assert user.password == @valid_attrs.password
       assert is_binary(user.password_hash)
     end
 
@@ -48,27 +40,23 @@ defmodule Garrahan.AccountsTest do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
     end
 
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
+    test "update_user/2 with valid data updates the user", %{user: user} do
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
-      assert user.email == "some updated email"
+      assert user.password == @update_attrs.password
       assert is_binary(user.password_hash)
     end
 
-    test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
+    test "update_user/2 with invalid data returns error changeset", %{user: user} do
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       are_same_user(Accounts.get_user!(user.id), user)
     end
 
-    test "delete_user/1 deletes the user" do
-      user = user_fixture()
+    test "delete_user/1 deletes the user", %{user: user} do
       assert {:ok, %User{}} = Accounts.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
 
-    test "change_user/1 returns a user changeset" do
-      user = user_fixture()
+    test "change_user/1 returns a user changeset", %{user: user} do
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
