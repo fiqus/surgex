0c4fdb313bc4fa89a12c15cad81b59f1c5bc9e28
MJoaaquin <joaquinmansilla@fiqus.com>
2019-02-07 14:51:11 -0300

    Create CustomTable component

---
+++ assets/js/components/custom-table.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <table>
+      <th
+        v-for="header in headers">
+          {{ header.value }}
+      </th>
+      <tr 
+        v-for="d in data">
+        <td
+          v-for="header in headers">
+            {{ d[header.key] }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      default: [],
+      required: true,
+    },
+    data: {
+      type: Array,
+      default: [],
+      required: true,
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
