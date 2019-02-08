a3194762253a54b25b7ccb330dc78ee149a35f4e
MJoaaquin <joaquinmansilla@fiqus.com>
2019-02-08 16:18:45 -0300

    Create customForm component

---
+++ assets/js/components/custom-form.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+		<h3 class="subtitle">Nuevo {{formName}} </h3>
+    <form>
+			<div v-for="field in fields">
+				<input type="text" :placeholder="field.placeholder" v-model="data[field.key]">
+			</div>
+			<button class="button" v-on:click="sendData">Crear</button>
+		</form>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+		formName: {
+			type: String,
+			required: true,
+			default: ""
+		},
+		fields: {
+			type: Array,
+			required: false,
+			default: []
+		},
+  },
+  data() {
+    return {
+			data: {}
+    }
+  },
+  methods: {
+		sendData() {
+			this.$emit("create", this.data);
+		}
+  }
+}
+</script>
